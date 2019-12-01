e5d2a31375f5517970e102ad2b86fd524ab3e78c
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2019-12-01 09:07:24 -0700

    day 1 part 1 add tests and benchmarks

---
+++ day1p1/main_test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"bytes"
+	"os"
+	"strings"
+	"testing"
+)
+
+func TestMain(t *testing.T) {
+	file, _ := os.Open("input.txt")
+	in = file
+	buff := new(bytes.Buffer)
+	out = buff
+
+	main()
+
+	result := strings.TrimSpace(buff.String())
+	if expected := "3368364"; expected != result {
+		t.Errorf("Expected %v, got %v", expected, result)
+	}
+}
+
+func BenchmarkMain(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		file, _ := os.Open("input.txt")
+		in = file
+		buff := new(bytes.Buffer)
+		out = buff
+
+		main()
+	}
+}
